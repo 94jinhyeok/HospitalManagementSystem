e21980a3e0b88713e673778757b76ca089dbafa0
chosun <chosun@chosun>
2018-11-12 14:10:02 +0900

    test

---
+++ src/main/Patient.java
@@ -0,0 +1,17 @@
+package main;
+import java.util.*;
+
+public class Patient {
+	Date inDate = new Date();
+	String Name = new String();
+	boolean sex;
+	int room;
+	String Protector = new String();
+	int receipt;
+	int tel;
+	char BloodType;
+	String Address = new String();
+	String Birth = new String();
+	int ManagementCode;
+	String P_Doctor = new String();
+}
